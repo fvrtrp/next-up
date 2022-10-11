560fc75ed867635d3e7de36fc7d6a45bf72e46ae
surajk95 <surajkukati95@gmail.com>
2022-10-11 21:06:53 +0530

    init

---
+++ scripts/content.js
@@ -0,0 +1,36 @@
+const trigger = document.createElement("div")
+trigger.id = "trigger-nextup"
+trigger.innerHTML = "Next Up"
+trigger.addEventListener('click', ()=>openMenu())
+
+document.body.appendChild(trigger)
+
+function openMenu() {
+    console.log(`menu opened`)
+}
+
+
+
+
+// const article = document.querySelector("article");
+
+// // `document.querySelector` may return null if the selector doesn't match anything.
+// if (article) {
+//   const text = article.textContent;
+//   const wordMatchRegExp = /[^\s]+/g; // Regular expression
+//   const words = text.matchAll(wordMatchRegExp);
+//   // matchAll returns an iterator, convert to array to get word count
+//   const wordCount = [...words].length;
+//   const readingTime = Math.round(wordCount / 200);
+//   const badge = document.createElement("p");
+//   // Use the same styling as the publish information in an article's header
+//   badge.classList.add("color-secondary-text", "type--caption");
+//   badge.textContent = `⏱️ ${readingTime} min read`;
+
+//   // Support for API reference docs
+//   const heading = article.querySelector("h1");
+//   // Support for article docs with date
+//   const date = article.querySelector("time")?.parentNode;
+
+//   (date ?? heading).insertAdjacentElement("afterend", badge);
+// }
